930c7228cf85807fd26768b4b3deff411d935e53
pavel <pal-sanych@tuta.io>
2016-07-10 16:58:32 +0300

    first commit

---
+++ default.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
+  "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+ 		<!--<meta name="discription" content="Правдолюбивое сообщество">	-->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  		<link href="http://palsanych.tk/PS5.ico" rel="icon" type="image/x-icon" />
+		<link href="http://palsanych.tk/PS5.ico" rel="shortcut icon" type="image/x-icon" />
+  		<title>Песочница</title>
+ 	</head>
+
+ 	<body bgcolor="teal">
+		<h1>Заголовок</h1>
+  		<!-- Комментарий -->
+  		<p>Первый абзац.</p>
+		<p>Второй абзац.</p>
+
+	</body>
+</html>
